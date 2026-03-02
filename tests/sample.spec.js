ce27edd06f8c2a797b020ca89ed275aa62252a02
aakashstrv03 <aakashstrv03@gmail.com>
2026-03-02 15:02:36 +0530

    Update sample.spec.js

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -3,3 +3,8 @@ test('Pagem', async ({ page }) => {
  await this.page.goto('https://www.amazon.com/')
 
 })
+test('Pagem', async ({ page }) => {
+
+ await this.page.goto('https://www.flipkart.com/')
+
+})
